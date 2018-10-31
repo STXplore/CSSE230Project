9d343fe1d7494fb79014c7a95adc29e9fcd8be4d
annisg <annisg@annisg-stg4.rose-hulman.edu>
2018-10-31 15:34:02 -0400

    Some basic stuff

--- CSSE230Project/src/Link.java
+++ CSSE230Project/src/Link.java
@@ -1,4 +1,19 @@
 
 public class Link {
 
+	public String key;
+	public double[] costs;
+	
+	public Link(String name) {
+		this.key = name;
+	}
+	
+	public String getKey() {
+		return key;
+	}
+	
+	public double getCost(int i) {
+		return costs[i];
+	}
+	
 }
