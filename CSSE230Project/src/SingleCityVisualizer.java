af780f5005b11a0bd899a0033e925f9c42b15375
jstensrud <jackstensrud@gmail.com>
2018-11-07 17:16:49 -0500

    visuals updated to add graph, also added interest pts to the file parser

--- CSSE230Project/src/SingleCityVisualizer.java
+++ CSSE230Project/src/SingleCityVisualizer.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
+import java.util.NoSuchElementException;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
@@ -14,32 +15,25 @@ import javax.swing.JTextField;
 public class SingleCityVisualizer extends JFrame{
 
 	
-	public SingleCityVisualizer(String cityName){
-		setTitle("City Info: " + cityName);
+	public SingleCityVisualizer(Graph g, Place city){
+		setTitle("City Info: " + city.getName());
 		setSize(400, 250);
 		JPanel content = new JPanel();
 		content.setLayout(new BoxLayout(content, BoxLayout.Y_AXIS));
 		
-		JLabel mainTitle = new JLabel("Information for " + cityName);
-		JLabel secondaryTitle = new JLabel("Popular Destinations");
-
-		JLabel distanceTitle = new JLabel("Closest city: " + "yeehaw junction");
-		JButton distanceSearch = new JButton("More Info");
+		JLabel mainTitle = new JLabel("Information for " + city.getName());
 		
-		JLabel timeTitle = new JLabel("Fastest city to get to: " + "cowboy town");
-		JButton timeSearch = new JButton("More Info");
+		JLabel timeTitle = new JLabel("Most Popular Point of Interest: " + city.getSinglePointOfInterest());
 
-		JTextField newCity = new JTextField("Find a route to any city");
+		JLabel searchTitle = new JLabel("Find a route to any city");
+		JTextField newCity = new JTextField();
 		JButton searchButton = new JButton("Search");
 		
 		JButton closeButton = new JButton("Main Menu");
 		
 		content.add(mainTitle);
-		content.add(secondaryTitle);
-		content.add(distanceTitle);
-		content.add(distanceSearch);
 		content.add(timeTitle);
-		content.add(timeSearch);
+		content.add(searchTitle);
 		content.add(newCity);
 		content.add(searchButton);
 		content.add(closeButton);
@@ -47,7 +41,7 @@ public class SingleCityVisualizer extends JFrame{
 		ActionListener close = new ActionListener(){
 			@Override
 			public void actionPerformed(ActionEvent e){
-				JFrame v = new Visualizer();
+				JFrame v = new Visualizer(g);
 				v.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 				v.setVisible(true);
 				SingleCityVisualizer.super.dispose();
@@ -62,37 +56,22 @@ public class SingleCityVisualizer extends JFrame{
 					JFrame v = new ErrorVisualizer();
 					v.setVisible(true);
 				} else{
-					JFrame v = new TwoCityVisualizer(cityName, newCity.getText());
+					try{
+					JFrame v = new TwoCityVisualizer(g, city, g.getPlace(newCity.getText()));
 					v.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 					v.setVisible(true);
 					SingleCityVisualizer.super.dispose();
+					}catch(NoSuchElementException ex){
+						JFrame v = new ErrorVisualizer();
+						v.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+						v.setVisible(true);
+					}
 				}
 			}
 		};
 		searchButton.addActionListener(search);
 
-		ActionListener distSearch = new ActionListener(){
-			@Override
-			public void actionPerformed(ActionEvent e){
-				JFrame v = new TwoCityVisualizer(cityName, "howdy");
-				v.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				v.setVisible(true);
-				SingleCityVisualizer.super.dispose();
-			}
-		};
-		distanceSearch.addActionListener(distSearch);
-		
-		ActionListener searchTime = new ActionListener(){
-			@Override
-			public void actionPerformed(ActionEvent e){
-				JFrame v = new TwoCityVisualizer(cityName, "yeehaw");
-				v.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				v.setVisible(true);
-				SingleCityVisualizer.super.dispose();
-			}
-		};
-		timeSearch.addActionListener(searchTime);
-		
+				
 		FocusListener newCityCheck = new FocusListener(){
 			@Override
 			public void focusGained(FocusEvent e){
